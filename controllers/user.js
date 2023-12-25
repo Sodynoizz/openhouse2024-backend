f15a52680761bc1990ad34e28c1197378b157623
Sodynoizz_TH <tepza5556789@gmail.com>
2023-12-26 00:12:16 +0700

    update: gems description

--- controllers/user.js
+++ controllers/user.js
@@ -16,14 +16,49 @@ const range = (x, min, max) => {
 };
 
 const gemRanges = [
-  { range: [46, 50], name: "แอเมทิสต์", description: "" },
-  { range: [41, 45], name: "บุษราคัม", description: ""  },
-  { range: [36, 40], name: "ไข่มุก", description: "" },
-  { range: [31, 35], name: "โทแพซ", description: "" },
-  { range: [26, 30], name: "ทับทิม", description: "" },
-  { range: [21, 25], name: "เพทาย", description: "" },
-  { range: [16, 20], name: "ไพลิน", description: "" },
-  { range: [11, 15], name: "ทัวร์มาลีน", description: "" },
+  { range: [46, 50], name: "แอเมทิสต์", description: "สีสันที่เฉพาะตัวของอัญมณีแอเมทิสต์อันแสนสวยงาม เป็นองค์แห่งสติปัญญาทั้งปวง ประดับประดาสัญชาตญาณของเหล่ามนุษย์ให้ปราดเปรื่อง และคอยช่วยผดุงไว้ซึ่งชัยชนะอันอำนวยด้วยขวัญแห่งอัญมณี" },
+  {
+    range: [41, 45],
+    name: "บุษราคัม",
+    description:
+      "“บุษราคัม” อัญมณีแห่งโชคลาภ ความสำเร็จที่ไม่มีวันโรยรา เปรียบเสมือนคำพูดที่ว่า destiny is destined by you มีเพียงคุณเท่านั้นที่เป็นผู้ลิขิตได้! ไม่ว่าจะเจอเรื่องหนักหนาเพียงใด เพียงเชื่อมั่นกับตัวเอง “ฉันทำได้” ไม่มีสิ่งใดจะพรากคุณจากความฝันไปได้ แล้วตัวคุณจะดึงดูดสิ่งดี ๆ เข้ามาหาคุณเอง\n\nเมื่อหัวใจของคุณสู้ พร้อมกับใช้หัวใจนำทาง โชคชะตาจะนำพาความสำเร็จมาให้คุณเอง",
+  },
+  {
+    range: [36, 40],
+    name: "ไข่มุก",
+    description:
+      "“ไข่มุก” ตัวแทนความบริสุทธิ์ผุดผ่อง นุ่มนวล สงบสุข และอ่อนหวาน ที่เป็นของขวัญจากท้องทะเลกว้างใหญ่ และด้วยความบริสุทธิ์ของไข่มุกสามารถช่วยรักษาสมดุลของอารมณ์ที่แปรปรวนไร้ความมั่นคงได้ ช่วยให้มีความอ่อนโยนและนุ่มนวล แต่ยังมั่นคงแข็งแกร่ง ยืนหยัดเพื่อตนเองอยู่เสมอ เปรียบเสมือนภายนอกที่มีความอ่อนหวานบริสุทธิ์ แต่ก็มีความมุ่งมั่นตั้งใจและรักที่จะไขว่คว้าเป้าหมายที่ตนปรารถนาอย่างไม่ลดละ",
+  },
+  {
+    range: [31, 35],
+    name: "โทแพซ",
+    description:
+      "“โทแพซ” อัญมณีแห่งความโชคดี เป็นอัญมณีที่มีความหมายอย่างหลากหลาย โดยมักจะสื่อถึงความแข็งแกร่งทางร่างกายและจิตใจ รวมถึงความมีชีวิตชีวา การมีความคิดสร้างสรรค์และแรงบันดาลใจเป็นเลิศ มีความสงบทางอารมณ์ ช่วยในการชี้แนะจุดมุ่งหมาย และยังส่งเสริมเรื่องของความเข้าใจลึกลงไปของจิตใจอีกด้วย",
+  },
+  {
+    range: [26, 30],
+    name: "ทับทิม",
+    description:
+      "“ทับทิม” มณีที่พ้องกับชื่อผลไม้นี้ ไม่ได้เป็นเพียงอัญมณีของความรักและอำนาจ แต่อัญมณีล้ำค่านี้ยังเป็นแสงสว่างให้ผู้เดินทาง คอยเปล่งแสงสีแดงแห่งชีวิต ปัดเป่าสิ่งชั่วร้ายและโรคภัยให้ไม่สามารถทำอันตรายได้ อัญมณีสีชาดนี้จะปกป้องและดลบันดาลความสำเร็จแก่ผู้ที่ครอบครอง\n\nด้วยความพยายามที่มอดไหม้ร้อนแรงดังเปลวประกายเพลิงนี้",
+  },
+  {
+    range: [21, 25],
+    name: "เพทาย",
+    description:
+      "“เพทาย” อัญมณีสีฟ้าสง่าน่าเกรงขาม เป็นที่รู้จักและแพร่หลายมาอย่างยาวนาน เป็นอัญมณีแห่งความโชคดี และสัญลักษณ์แห่งความฉลาด เครื่องรางป้องกันความชั่วร้ายและก้าวไปสู่ความสำเร็จ ดึงดูดผู้ที่มีความใฝ่ฝัน กล้าที่จะเดินตามความฝันและมุ่งมั่นที่จะทำฝันนั้นให้เป็นจริง\n\nอัญมณีนี้กำลังดึงดูดคนที่มีความใฝ่ฝัน มีความกล้าที่จะเดินตามความฝันและมุ่งมั่นที่จะทำฝันนั้นให้เป็นจริง",
+  },
+  {
+    range: [16, 20],
+    name: "ไพลิน",
+    description:
+      "“ไพลิน” อัญมณีสีนํ้าเงินแกมม่วงหรือนํ้าเงินสดมีประกายแวววาวที่จะพาคุณเปล่งประกายระยิบระยับไปกับความฝันของตัวเอง ไพลินจะเป็นตัวช่วยให้คุณมีสมาธิ แน่วแน่ ปกป้องภัยร้ายและช่วยให้ประสบความสำเร็จ พร้อมค้นหาความเป็นตัวของตัวเองเสมือนไพลินหลากหลายรูปแบบที่ถูกค้นพบและนิยามความหมายไว้ต่างกันตามแหล่งที่เจอ",
+  },
+  {
+    range: [11, 15],
+    name: "ทัวร์มาลีน",
+    description:
+      "“ทัวร์มาลีน” เปรียบดั่งความฝันอันแสนสวยงามในโลกแห่งอัญมณี ทุกคนล้วนมีความฝัน และความฝันจะเป็นจริงได้ก็ต้องอาศัยความหวังและความพยายาม เฉกเช่นการเจียระไน เพื่อสรรสร้างความฝันนั้นให้เป็นจริง ทัวร์มาลีนของคุณจะกลายเป็นอัญมณีที่ล้ำค่าและถูกเจียระไนเป็นรูปทรงที่ไม่เหมือนใคร เปรียบดั่งผลลัพธ์แห่งความตั้งใจและความพยายามของทุกคนที่จะออกมาสวยงามในรูปแบบของตนเอง\n\nเพียงแค่คุณมีความฝัน ทัวร์มาลีนนั้นก็จะมาอยู่ในครอบครองของคุณ . . .",
+  },
 ];
 
 export const addToDB = async (req, res) => {
@@ -89,19 +124,19 @@ export const getUserInfo = async (req, res) => {
   // if (!CheckEnvironmentKey(environmentKey) {
   //   return sendResponse(res, 400, "Environment key doesn't match");
   // }
-    
+
   try {
     const user = await userModel.findOne({ email: email });
     if (user) {
       return sendResponse(res, 200, user);
     } else {
-      return sendResponse(res, 404, "User not found"); 
+      return sendResponse(res, 404, "User not found");
     }
   } catch (err) {
     console.log(err);
     return sendResponse(res, 500, "Internal Server Error");
   }
-}
+};
 
 export const registerUser = async (req, res) => {
   const { email, environmentKey } = req.body;
@@ -157,8 +192,8 @@ export const UpdateScore = async (req, res) => {
 
     if (!score) {
       return sendResponse(res, 400, "Invalid Score Field");
-    } 
-    
+    }
+
     if (user) {
       const matchingGem = gemRanges.find((gem) => range(score, ...gem.range));
       gems = matchingGem ? matchingGem.name : undefined;
@@ -204,7 +239,7 @@ export const GetGems = async (req, res) => {
 
 export const AddStaff = async (req, res) => {
   const { id, organizationName, tag, environmentKey } = req.body;
-  
+
   // if (!CheckEnvironmentKey(environmentKey)) {
   //   return sendResponse(res, 400, "Environment key doesn't match");
   // }
@@ -212,8 +247,8 @@ export const AddStaff = async (req, res) => {
   try {
     const user = await userModel.findOne({ id: id });
     const staff = {
-      "organizationName": organizationName,
-      "tag": tag
+      organizationName: organizationName,
+      tag: tag,
     };
 
     if (user) {
@@ -228,7 +263,7 @@ export const AddStaff = async (req, res) => {
     console.log(err);
     return sendResponse(res, 500, "Internal Server Error");
   }
-}
+};
 
 export const GetStaffInfo = async (req, res) => {
   const { id, environmentKey } = req.body;
@@ -241,9 +276,9 @@ export const GetStaffInfo = async (req, res) => {
     const user = await userModel.findOne({ id: id });
     if (user) {
       const staff = {
-        "isstaff": user.isstaff,
-        "organizationName": user.organizationName,
-        "staff": user.staff
+        isstaff: user.isstaff,
+        organizationName: user.organizationName,
+        staff: user.staff,
       };
       return sendResponse(res, 200, staff);
     } else {
@@ -253,4 +288,4 @@ export const GetStaffInfo = async (req, res) => {
     console.log(user);
     return sendResponse(res, 500, "Internal Server Error");
   }
-}
+};
