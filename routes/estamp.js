ddefd9f536bff2d135fb9594073a9c65659e9748
pr1gram <139366091+pr1gram@users.noreply.github.com>
2024-01-08 16:47:47 +0700

    update: reset estamp

--- routes/estamp.js
+++ routes/estamp.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { UpdateEstamp, GetEstampUser } from "../controllers/estamp.js";
+import { UpdateEstamp, GetEstampUser, ResetEstampUser } from "../controllers/estamp.js";
 
 const estampRouter = Router();
 
 estampRouter.post("/update", UpdateEstamp);
 estampRouter.post("/get", GetEstampUser);
+estampRouter.post("/reset", ResetEstampUser);
 
 export default estampRouter;
